0dda482ded36416eedca20504a19b427d8cc8b98
Carlo Caldura <79699017+CarloCaldura@users.noreply.github.com>
2023-01-22 15:35:35 +0100

    Update Esercizio_week6.py

--- GruppoB/Week6/Carlo_Caldura/Esercizio_week6.py
+++ GruppoB/Week6/Carlo_Caldura/Esercizio_week6.py
@@ -1,1 +1,4 @@
+##TODO Trovare l'anno all'interno della stringa s2 in modo indipendente dalla posizione dell'anno all'interno della stringa
 
+s2 = "REDDITO_LAVORO_2021_DIPENDENTE"
+print(s2.join([anno for anno in s2.split("_") if anno.isdigit()]))
