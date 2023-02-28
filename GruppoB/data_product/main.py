ae2547e2bb5cb82b29ebf01456dc51ce6ca8fee4
DavidePosillipo <davide.posillipo@icloud.com>
2023-02-28 11:46:09 +0100

    aggiunto materiale su numpy week12

--- GruppoB/data_product/main.py
+++ GruppoB/data_product/main.py
@@ -1,5 +1,6 @@
 from src.DataIngestor import DataIngestor
 
 di = DataIngestor()
-
+m = di.retrieve_single_movie('8012')
+print(m)
 di.say_hello()
--- GruppoB/data_product/src/DataIngestor.py
+++ GruppoB/data_product/src/DataIngestor.py
@@ -1,7 +1,14 @@
+from imdb import Cinemagoer
+
 class DataIngestor:
 
     def __init__(self):
-        pass
+        self.cinemagoer_istance = Cinemagoer()
+        print(self.cinemagoer_istance)
+
+    def retrieve_single_movie(self, id):
+        movie = self.cinemagoer_istance.get_movie(id)
+        return movie
 
     def say_hello(self):
         print("hello world")
